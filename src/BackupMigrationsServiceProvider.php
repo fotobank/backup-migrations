99373ebf874cd789d9a84bc4cb26bfd00098af43
James Cook <jamescook@gmlconsulting.co.uk>
2018-06-12 14:16:55 +0100

    initial commit

---
+++ src/BackupMigrationsServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Pangolinkeys\BackupMigrations;
+
+use Illuminate\Support\ServiceProvider;
+
+class BackupMigrationsServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+        
+    }
+}
