e3f2ca404356d7735937d0bc74b9314476730f1e
Jurii <aleksjurii@gmail.com>
2019-12-08 00:25:58 +0200

    Update filesystem.php

--- config/filesystem.php
+++ config/filesystem.php
@@ -1,15 +1,9 @@
 <?php
 return [
     'disks' => [
-	    'backup' => [
-		    'driver' => 'local',
-		    'root' => storage_path('mysql_dump'),
-		    'url' => env('APP_URL').'/storage',
-		    'visibility' => 'public',
-	    ],
         'backup-migrations' => [
             'driver' => 'local',
-            'root' => storage_path('mysql_dump').'/'.config('backup-migrations.path'),
+            'root' => config('filesystems.disks.backup.root').'/'.config('backup-migrations.path'),
         ],
     ],
 ];
